9918d8a44da571a09173ab61ad4ec4c4cde35026
Meg MacIver <megmaciver13@gmail.com>
2022-07-04 18:10:57 -0500

    creates initial vue setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="mmm-logo" src="./assets/logo.png">
+  <HelloWorld msg="Meg Mansour"/>
 </template>
 
 <script>
@@ -22,5 +22,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  .mmm-logo {
+    max-width: 5rem;
+    width: 100%;
+  }
 }
+
+
 </style>
