5dacf9d37168c0bfdc002f29af22c631675f5d79
Meg MacIver <megmaciver13@gmail.com>
2022-07-04 19:38:07 -0500

    installs bootstrap

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,38 @@
 <template>
-  <h1>{{ name }}</h1>
-  <div class="location">
-    based in Austin, TX
-  </div>
-  <div class="brand-statement">
-    <p>
-      A mission-driven web developer, I am a pro at iterating and pivoting as project needs evolve, and I am committed to finding creative solutions.
-    </p>
-  </div>
+  <section>
+    <div class="container">
+
+      <div class="row">
+        <h1>{{ name }}</h1>
+      </div>
+      <div class="row title">
+        <div class="col">
+          web developer
+        </div>
+      </div>
+      <div class="row location">
+        <div class="col">
+          based in Austin, TX
+        </div>
+      </div>
+      <div class="row brand-statement">
+        <div class="col-md-8 offset-md-2">
+          <p>
+            A mission-driven web developer, I am a pro at iterating and pivoting as project needs evolve, and I am committed to finding creative solutions.
+          </p>
+        </div>
+      </div>
+      <div class="row social-media justify-content-center">
+        <div class="col-2">
+          <a href="https://www.linkedin.com/in/meg-mansour/" target="_blank">LinkedIn</a>
+        </div>
+        <div class="col-2">
+          <a href="https://github.com/megmaciver13" target="_blank">GitHub</a>
+        </div>
+      </div>
+
+    </div>
+  </section>
 </template>
 
 <script>
